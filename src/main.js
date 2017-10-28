f2f766e8e599a4a309082ecceecaeaf03f0b743a
Jack <jackmead515@gmail.com>
2017-10-28 11:23:47 -0500

    First commit

--- src/main.js
+++ src/main.js
@@ -10,20 +10,13 @@ import config from './config'
 
 class Game extends Phaser.Game {
   constructor () {
-    const docElement = document.documentElement
-    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
-    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
-
-    super(width, height, Phaser.CANVAS, 'content', null)
+    super(config.gameWidth, config.gameHeight, Phaser.AUTO, 'content', null)
 
     this.state.add('Boot', BootState, false)
     this.state.add('Splash', SplashState, false)
     this.state.add('Game', GameState, false)
 
-    // with Cordova with need to wait that the device is ready so we will call the Boot state in another file
-    if (!window.cordova) {
-      this.state.start('Boot')
-    }
+    this.state.start('Boot')
   }
 }
 
@@ -39,12 +32,8 @@ if (window.cordova) {
       )
     },
 
-    // deviceready Event Handler
-    //
     onDeviceReady: function () {
       this.receivedEvent('deviceready')
-
-      // When the device is ready, start Phaser Boot state.
       window.game.state.start('Boot')
     },
 
@@ -52,6 +41,5 @@ if (window.cordova) {
       console.log('Received Event: ' + id)
     }
   }
-
   app.initialize()
 }
